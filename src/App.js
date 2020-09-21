c7935c39442921b7314b58ac0b5702b42371df3f
max <xiaoxin88420@hotmail.com>
2020-09-21 11:24:37 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
-import Porfolio from './pages/Portfolio'
+import Portfolio from './pages/Portfolio'
 import Contact from './pages/Contact'
 import {
   Collapse,
