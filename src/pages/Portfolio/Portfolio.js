096c0d149739dd7d787e081ca9d22b20f884b924
max <xiaoxin88420@hotmail.com>
2020-09-22 21:32:49 -0700

    update

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import NutraPrep from './pics/firstproject.jpg'
+import RanchHand from './pics/secondproject.jpg'
+import EDaBurger from './pics/e-da-burger.jpg'
+import weatherApp from './pics/weatherapp.jpg'
+import workdayScheduler from './pics/work-day-scheduler.jpg'
 import {
   Card,
   CardImg,
@@ -19,12 +24,23 @@ const Portfolio = () => {
         <Row>
           <Col>
             <Card>
-              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardImg top width="100%" src={NutraPrep} alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
+                <CardTitle>NutraPrep</CardTitle>
+                <CardSubtitle>First Project</CardSubtitle>
+                <CardText>This is my first group project of using third party API to retrieve nutricious recipe when searching by keywords.</CardText>
+                <Button><a href='https://nicholasd-uci.github.io/manhattanProject/'>NutraPrep</a></Button>
+              </CardBody>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <CardImg top width="100%" src={RanchHand} alt="Card image cap" />
+              <CardBody>
+                <CardTitle>Ranch-Hand</CardTitle>
+                <CardSubtitle>Second Project</CardSubtitle>
+                <CardText>This is my second group project of building up our own database by using MySQL as well as setting up routers to practice 'GET', 'POST', 'PUT' and 'DELETE' on data.</CardText>
+                <Button><a href='https://gentle-taiga-08855.herokuapp.com/index.html'>Ranch-Hand</a></Button>
               </CardBody>
             </Card>
           </Col>
@@ -32,56 +48,46 @@ const Portfolio = () => {
             <Card>
               <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button><a href=''></a></Button>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col>
-            <Card>
-              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
-              <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                <CardTitle>Coming soon</CardTitle>
+                <CardSubtitle>Third Project</CardSubtitle>
+                <CardText>This is my third group project. We're planning to build an app for users to find party catering/dj/entertainment supplies all in one stop.</CardText>
                 <Button>Button</Button>
               </CardBody>
             </Card>
           </Col>
         </Row>
+        <hr/>
         <Row>
           <Col>
             <Card>
-              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardImg top width="100%" src={EDaBurger} alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
+                <CardTitle>E-Da-Burger</CardTitle>
+                <CardSubtitle>Homework</CardSubtitle>
+                <CardText>This is a piece of homework to practice building up a local data and maintain it by using 'CRUD' method.</CardText>
+                <Button><a href='https://e-da-burger.herokuapp.com/'>E-Da-Burger</a></Button>
               </CardBody>
             </Card>
           </Col>
           <Col>
             <Card>
-              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardImg top width="100%" src={weatherApp} alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
+                <CardTitle>WeatherApp</CardTitle>
+                <CardSubtitle>Homework</CardSubtitle>
+                <CardText>This is an app that could post the weather for the future five days of a chosen city.</CardText>
+                <Button><a href='https://e-da-burger.herokuapp.com/'>WeatherApp</a></Button>
               </CardBody>
             </Card>
           </Col>
           <Col>
             <Card>
-              <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+              <CardImg top width="100%" src={workdayScheduler} alt="Card image cap" />
               <CardBody>
-                <CardTitle>Card title</CardTitle>
-                <CardSubtitle>Card subtitle</CardSubtitle>
-                <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-                <Button>Button</Button>
+                <CardTitle>Workday Scheduler</CardTitle>
+                <CardSubtitle>Homework</CardSubtitle>
+                <CardText>This is an app that can save your input event and the color will change by time as a reminder.</CardText>
+                <Button><a href='https://xiaoxin88420.github.io/WorkScheduler/'>Workday Scheduler</a></Button>
               </CardBody>
             </Card>
           </Col>
