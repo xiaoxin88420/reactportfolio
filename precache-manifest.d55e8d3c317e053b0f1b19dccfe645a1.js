547148ba35d2d140572bcdd7472911aaf588721c
max <xiaoxin88420@hotmail.com>
2020-09-23 21:30:26 -0700

    Updates

---
+++ precache-manifest.d55e8d3c317e053b0f1b19dccfe645a1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1659af8d260e608c4ed8698995e014",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "b15ea40a00cd917c889f",
+    "url": "/reactportfolio/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "0004b380eca14095e883",
+    "url": "/reactportfolio/static/css/main.6eda1502.chunk.css"
+  },
+  {
+    "revision": "b15ea40a00cd917c889f",
+    "url": "/reactportfolio/static/js/2.75aae121.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reactportfolio/static/js/2.75aae121.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0004b380eca14095e883",
+    "url": "/reactportfolio/static/js/main.36b62edf.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "229366017a068340d75b0828a4d8b43d",
+    "url": "/reactportfolio/static/media/aboutme.22936601.jpg"
+  },
+  {
+    "revision": "166d6653b1199259cd240d95b677af80",
+    "url": "/reactportfolio/static/media/e-da-burger.166d6653.jpg"
+  },
+  {
+    "revision": "9bbc30c7de81ad80343fb2a949041094",
+    "url": "/reactportfolio/static/media/firstproject.9bbc30c7.jpg"
+  },
+  {
+    "revision": "35b0ea5db8ccba3c4639651bc0291407",
+    "url": "/reactportfolio/static/media/modifyport1.35b0ea5d.jpg"
+  },
+  {
+    "revision": "a6ebc8568953d2323f8d37bd00453e40",
+    "url": "/reactportfolio/static/media/port2.a6ebc856.jpg"
+  },
+  {
+    "revision": "8263d45a0a6e1ea45fc3b1554db2061e",
+    "url": "/reactportfolio/static/media/port3.8263d45a.jpg"
+  },
+  {
+    "revision": "80e3340d1e7bc0f78cca62005842ac4a",
+    "url": "/reactportfolio/static/media/psgen.80e3340d.jpg"
+  },
+  {
+    "revision": "ae40d4f0c2efda083acb62952a275f10",
+    "url": "/reactportfolio/static/media/secondproject.ae40d4f0.jpg"
+  },
+  {
+    "revision": "a1b32b8822ca2a97aec7dd50073d4a4f",
+    "url": "/reactportfolio/static/media/weatherapp.a1b32b88.jpg"
+  },
+  {
+    "revision": "03e1c009560e7a190145168e36d862ec",
+    "url": "/reactportfolio/static/media/work-day-scheduler.03e1c009.jpg"
+  }
+]);
